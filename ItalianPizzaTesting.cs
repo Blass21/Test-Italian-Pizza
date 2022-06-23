0aa9fc54427425cb322ae60cd0371d2e42130954
JavierBlas <zs19014035@estudiantes.uv.mx>
2022-06-22 19:33:01 -0500

    First commit

---
+++ ItalianPizzaTesting.cs
@@ -0,0 +1,39 @@
+﻿using Backend.Contracts;
+using Backend.Models;
+using Backend.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestItalianPizza
+{
+    [TestClass]
+    public class ItalianPizzaTesting
+    {
+        //private readonly IItalianPizzaService service;
+
+        private ItalianPizzaContext context;
+   
+        [TestInitialize]
+        public void Initialize()
+        {
+            context = new ItalianPizzaContext(Effort.DbConnectionFactory.CreateTransient());
+        }
+
+        [TestMethod]
+        public void GetCustomersListTest()
+        {
+            //Arrange
+            var italianPizza = new ItalianPizzaTest(context);
+            //string nameFake = "pedro";
+            List<CustomerContract> customerList = new List<CustomerContract>();
+            var customerListTest = italianPizza.GetCustomerListSortedByName();
+
+            //Act
+            //Assert.IsTrue(customerList.Count > 0);
+            Assert.IsTrue(true);
+            //Assert
+        }
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestItalianPizza")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestItalianPizza")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eef34f4f-3e5c-4f77-ad14-5dd3f4153859")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
